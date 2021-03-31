77b1352a3dc57c13636b477771ffd6b43b99d5f5
Jack-Moon628 <bright_96_628@outlook.com>
2021-03-31 22:54:24 +0800

    hero component created

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit {
+
+  constructor() { }
+  hero: string = "name hero";
+  ngOnInit(): void {
+  }
+
+}
